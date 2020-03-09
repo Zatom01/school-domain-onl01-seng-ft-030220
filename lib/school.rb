ef49fc9213d3640c53c33017a4acc6d34a6846c4
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-09 22:06:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  
+  
+  
+  def initialize(name)
+    @name=name
+  end
+
+  def roster
+    @roster=Hash.new
+  end 
+    
+  def add_student(student,grade)
+    @roster[student] << grade
+  end
+    
+  
+end 
