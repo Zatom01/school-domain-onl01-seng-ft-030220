f8c2f453e49cd2bba29b20ecc3dce3cdd7d679c7
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-09 22:23:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,10 +10,12 @@ class School
   end 
     
   def add_student(student,grade)
-    if @roster[grade] != nil
-      @roster[grade] << studnet
-    else
+    if @roster[grade] ==[]
       @roster[grade] = [student]
+    else
+      @roster[grade] << student
+    
+      
     end
     
   end
